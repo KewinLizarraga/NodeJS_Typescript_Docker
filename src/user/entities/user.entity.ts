5454100a4795b34943fae718afe85d53b7e60663
kLizarraga <kewin392@gmail.com>
2022-09-29 15:39:08 -0500

    entities added

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,30 @@
+import { Column, Entity, OneToOne } from "typeorm";
+import { BaseEntity } from "../../config/base.entity";
+import { CustomerEntity } from "../../customer/entities/customer.entity";
+
+@Entity({ name: "user" })
+export class UserEntity extends BaseEntity {
+  @Column({ length: 1000 })
+  name!: string;
+
+  @Column()
+  lastname!: string;
+
+  @Column()
+  username!: string;
+
+  @Column()
+  email!: string;
+
+  @Column()
+  passowrd!: string;
+
+  @Column()
+  city!: string;
+
+  @Column()
+  province!: string;
+
+  @OneToOne(() => CustomerEntity, (customer) => customer.user)
+  customer!: CustomerEntity;
+}
